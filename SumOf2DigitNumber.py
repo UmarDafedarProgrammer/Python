3231b7f912c25fbf6414f95547643f2e182797f9
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-23 22:33:45 +0530

    Create SumOf2DigitNumber.py

---
+++ SumOf2DigitNumber.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+inpstr = str(two_digit_number)
+# firstdigit = inpstr[0]
+# seconddigit = inpstr[1]
+
+print(int(inpstr[0])+int(inpstr[1]))
