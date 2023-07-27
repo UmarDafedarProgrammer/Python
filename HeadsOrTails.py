7e7dedb19e7b6fdb4309b451220c9ad57707b1d8
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-27 06:20:37 +0530

    Create HeadsOrTails.py

---
+++ HeadsOrTails.py
@@ -0,0 +1,12 @@
+#Remember to use the random module
+#Hint: Remember to import the random module here at the top of the file. 🎲
+	 
+#Write the rest of your code below this line 👇
+import random
+
+HorT = random.randint(0,1)
+
+if HorT == 0:
+    print("Tails")
+else:
+    print("Heads")
