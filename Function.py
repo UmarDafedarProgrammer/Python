aa30f8ef1dd86e91975f1fd778ccba114af7f507
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-29 20:19:00 +0530

    Update Function.py

--- Function.py
+++ Function.py
@@ -8,4 +8,11 @@ def greet():
   print("How are You!")
   print("How may I help you ?")
 
+def greet_with_name(name):
+  print(f"Hello, {name}!")
+  print(f"How are You,{name}!")
+  print("How may I help you ?")
+  
+
 greet()
+greet_with_name("Robert")
