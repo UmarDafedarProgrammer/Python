6fd846471470fe0969b5a6c0ae572d1f65ced27f
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-30 01:53:41 +0530

    Update TravelLogUsingDictAndList.py

--- TravelLogUsingDictAndList.py
+++ TravelLogUsingDictAndList.py
@@ -15,12 +15,18 @@ travel_log = [
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
 def add_new_country(country,NumOfVisits,CityList):
-    travel_log.append(
-        {
-            "country":country,
-            "visits": NumOfVisits,
-            "cities": CityList
-        })
+    # travel_log.append(
+    #     {
+    #         "country":country,
+    #         "visits": NumOfVisits,
+    #         "cities": CityList
+    #     })
+  new_country = {}
+  new_country["country"] = country
+  new_country["visits"] = NumOfVisits
+  new_country["cities"] = CityList
+  travel_log.append(new_country)
+  
 
 
 
