75e5e313aac5a77129ac8b7f5a36617cd9b18606
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-07-29 20:13:05 +0530

    Create Function.py

---
+++ Function.py
@@ -0,0 +1,11 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+
+def greet():
+  print("Hello!")
+  print("How are You!")
+  print("How may I help you ?")
+
+greet()
