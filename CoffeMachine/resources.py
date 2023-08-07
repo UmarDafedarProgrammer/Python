f0de5499749f6ef04392942da8315fb97598d83d
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-08-08 03:36:20 +0530

    Create resources.py

---
+++ CoffeMachine/resources.py
@@ -0,0 +1,31 @@
+resources = {
+    "milk": 5000,
+    "water": 2500,
+    "coffee": 1000,
+    "honey": 500,
+    "money": 50
+}
+
+recipe = {
+    "Espresso": {
+        "milk": 0,
+        "coffee": 18,
+        "water": 50,
+        "honey": 10,
+        "price": 1.5
+    },
+    "Cappuccino": {
+        "milk": 100,
+        "coffee": 24,
+        "water": 250,
+        "honey": 10,
+        "price": 3
+    },
+    "latte": {
+        "milk": 150,
+        "coffee": 24,
+        "water": 200,
+        "honey": 10,
+        "price": 2.5
+    }
+}
