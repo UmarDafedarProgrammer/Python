1e94bff7e340e7ef23ebf12c05023a96820f9d37
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-08-11 00:56:35 +0530

    Update Turtle_Shapes.py

--- Turtle_Shapes.py
+++ Turtle_Shapes.py
@@ -6,10 +6,11 @@ turt.shape("turtle")
 k = 0
 length = 200
 color = ["red","green","yellow","pink","black"]
-for i in range(3,8):
-  angle = 360/i;
+for sides in range(3,8):
+  angle = 360/sides;
   turtle.color(color[k])
   k += 1
-  for i in range(0,i):
+  while sides>0:
     turtle.forward(length)
     turtle.right(angle)
+    sides -= 1
