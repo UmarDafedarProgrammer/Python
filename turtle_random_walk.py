b78598d01b015bcbf4ad93473982bba9be7fc31b
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-08-11 01:26:41 +0530

    Update turtle_random_walk.py

--- turtle_random_walk.py
+++ turtle_random_walk.py
@@ -1,6 +1,15 @@
 import turtle as t
 import random
 
+t.colormode(255)
+
+def random_color():
+  r = random.randint(0,255)
+  g = random.randint(0,255)
+  b = random.randint(0,255)
+  rcolor = (r,g,b)
+  return rcolor
+
 trt = t.Turtle()
 trt.speed("fastest")
 NumberOfSteps = int(input("Please, enter the number of steps: "))
@@ -12,7 +21,7 @@ for i in range(0,NumberOfSteps):
   distance = random.randint(50,100)
   col_index = random.randint(0,3)
   dir_index = random.randint(0,3)
-  t.color(colors[col_index])
+  t.color(random_color())
   if dir_index == 0:
     pass
   elif dir_index == 1:
