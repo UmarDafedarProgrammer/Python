b39eed0391bbc432e83edf27d699b152c4bc969c
Umar Dafedar <108915275+UmarDafedarProgrammer@users.noreply.github.com>
2023-08-13 08:29:52 +0530

    Update main.py

--- MailMerger/main.py
+++ MailMerger/main.py
@@ -6,3 +6,37 @@
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
         #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+INVITEES = "Input/Names/invited_names.txt"
+EMAIL_FORMAT = "Input/Letters/starting_letter.txt"
+OUTPUT_FOLDER = "Output/ReadyToSend"
+PLACEHOLDER = "[name]"
+
+
+##### Read the list of invitees
+def read_invitees():
+  with open(INVITEES, mode="r") as invitees:
+    invitees_list = invitees.readlines()
+    return invitees_list
+
+
+##### Read the content of the letter
+def email_content():
+  print("*********Email Content!")
+  with open(EMAIL_FORMAT) as letter:
+    content = letter.read()
+    return content
+
+
+##### Generate mailers
+def generate_mails():
+  list = read_invitees()
+  content = email_content()
+  for invitee in list:
+    file = open(f"{OUTPUT_FOLDER}/InvitationLetter_{invitee}.txt", mode="w")
+    stripped_name = invitee.strip()
+    new_content = content.replace(PLACEHOLDER, stripped_name)
+    file.write(new_content)
+    file.close()
+
+
+generate_mails()
